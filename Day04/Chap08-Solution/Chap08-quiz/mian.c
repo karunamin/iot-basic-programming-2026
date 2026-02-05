fc29ced57c63ab7d70551e0458d003c5cd61601b
karunamin <freedom12528@gmail.com>
2026-02-05 17:29:41 +0900

    4일차 커밋

---
+++ Day04/Chap08-Solution/Chap08-quiz/mian.c
@@ -0,0 +1,36 @@
+﻿// 대소문자 변환 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char str[80];
+	int i;
+	int count = sizeof(str) / sizeof(str[0]);
+	int pos;	// 문자열에서\0값이 처음 나오는 위치(배열 인덱스)
+	int num = 0; //대문자에서 소문자로 바뀐 갯수
+	
+	printf("문장 입력 : ");
+	gets(str);
+
+	for (i = 0; i < count; i++) {
+		if (str[i] == '\0') {
+			pos = i;
+			break;
+		}
+	}
+	//printf("%d\n", pos);
+
+	for (i = 0; i < pos; i++) {
+		if(str[i]>='A'&& str[i]<'Z') {//문자 한자가 A에서 Z사이에 있으면
+			str[i] = str[i] + 32;	//소문자 변환
+			num++;
+		}
+	}
+	printf("바뀐문장 : ");
+	puts(str);
+	printf("바뀐 문자 갯수 : %d\n", num);
+
+
+	return 0;
+}
---
+++ Day04/Chap08-Solution/ConsoleApplication1/main.c
@@ -0,0 +1,26 @@
+﻿// 대소문자 변환 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+	char str[80];
+	int i;
+	int count = sizeof(str) / sizeof(str[0]);
+	int pos; // 문자열에서 \0값이 처음 나오는 위치(배열 인덱스)
+
+	printf("문자열 입력 : ");
+	gets(str);		// DON'T Worry, Be Happy~ --> don't warry, be happy => 바뀐 문자가 7
+
+	for (i = 0; i < count; i++) {
+		if (str[i] == '\0') {
+			pos = i;
+			break;
+		}
+	}
+
+
+
+	puts(str);
+}
